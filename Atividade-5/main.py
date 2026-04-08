842cf8cb63be25f777529f28b3e3be90795f91d1
Camila <camisalgado2005@gmail.com>
2026-04-08 01:38:35 -0300

    adição música batman e nuvens

--- Atividade-5/main.py
+++ Atividade-5/main.py
@@ -10,12 +10,12 @@ batman_pixel = transform.scale(batman_pixel, (150, 150))
 
 batman_font = font.Font("./Atividade-5/batmfa__.ttf")
 
-# mixer.music.load() # check myinstants website
-# mixer.music.play(-1)
+mixer.music.load("./Atividade-5/batman-song-1966.mp3")
+mixer.music.play(-1)
 
 window = display.set_mode((1280, 720))
 
-window.fill((151, 209, 250))
+window.fill((31, 51, 130))
 
 while True:
     for ev in event.get():
@@ -24,15 +24,31 @@ while True:
             sys.exit()
 
     # Casa
-    draw.rect(window, (72, 157, 37), (0, 600, 1280, 120)) # chão
+    draw.rect(window, (2, 64, 0), (0, 600, 1280, 120)) # chão
     draw.rect(window, (100, 100, 100), (320, 360, 240, 240)) # casa
-    draw.rect(window, (13, 22, 100), (350, 456, 60, 96)) # janela
-    draw.rect(window, (121, 77, 27), (450, 440, 80, 160)) # porta
-    draw.polygon(window, (242, 136, 59), ((300, 360), (440, 240), (580, 360)))
+    draw.rect(window, (201, 165, 34), (350, 456, 60, 96)) # janela
+    draw.rect(window, (56, 56, 56), (450, 440, 80, 160)) # porta
+    draw.polygon(window, (0, 0, 0), ((300, 360), (440, 240), (580, 360)))
     draw.circle(window, (0, 0, 0), (460, 520), 5)
 
-    # Sol
-    # draw.line(window, (255, 242, 81), (50, 50), (150, 150), 6)
+    # Árvore
+    draw.rect(window, (84, 41, 3), (960, 400, 80, 200)) # tronco
+    draw.circle(window, (0, 48, 2), (1000, 350), 90)
+
+    # Lua
+    draw.circle(window, (140, 178, 194), (200, 150), 100)
+    draw.circle(window, (31, 51, 130), (230, 150), 100)
+
+    # Nuvem
+    def nuvem(x, y, size):
+        for i in range(2):
+            draw.circle(window, (164, 174, 179), (x+i*(size + 10)*3, y), size)
+        for i in range(2):
+            draw.circle(window, (164, 174, 179), (x+50+i*(size + 10), y), size+10)
+
+    nuvem(900, 100, 50)
+    nuvem(600, 150, 35)
+
 
     # Imagem
     window.blit(batman_logo, (380, 260))
@@ -41,5 +57,3 @@ while True:
     window.blit(batman_text, (600, 200))
 
     display.update()
-
-    # 320, 320, 
