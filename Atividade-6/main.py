7db62b8e2ea1297b73b9486c09bf0cb953380632
Camila <camisalgado2005@gmail.com>
2026-04-13 12:40:23 -0300

    atividade 6

--- Atividade-6/main.py
+++ Atividade-6/main.py
@@ -8,15 +8,33 @@ clock = time.Clock()
 
 # Variáveis
 pos_x = 0
+bg_color = '#000000'
 
 while running:
     clock.tick(60)
+    keys = key.get_pressed()
 
     for ev in event.get():
         if ev.type == QUIT:
             running = False
+        if ev.type == KEYDOWN:
+            pressed = ev.key
+            if pressed == K_SPACE:
+                if bg_color == '#000000':
+                    bg_color = ((255, 255, 255))
+                elif bg_color == (255, 255, 255):
+                    bg_color = ('#000000')
 
+    # Update
     dt = clock.get_time()/1000
+
+    if keys[K_d] or keys[K_RIGHT]:
+        pos_X = pos_x + 1 * dt
+    elif keys[K_a] or keys[K_LEFT]:
+        pos_X = pos_x - 1 * dt
+
+    # Draw
+    window.fill(bg_color)
 
 
     display.update()
