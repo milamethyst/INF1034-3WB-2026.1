a6cf3ac7dafec3859829299e35e5979ad6f43ce5
Camila <camisalgado2005@gmail.com>
2026-04-13 12:53:02 -0300

    press mouse

--- Atividade-6/main.py
+++ Atividade-6/main.py
@@ -9,10 +9,13 @@ clock = time.Clock()
 # Variáveis
 pos_x = 0
 bg_color = '#000000'
+text = ''
+
 
 while running:
     clock.tick(60)
     keys = key.get_pressed()
+    mouse_buttons = mouse.get_pressed()
 
     for ev in event.get():
         if ev.type == QUIT:
@@ -24,9 +27,14 @@ while running:
                     bg_color = ((255, 255, 255))
                 elif bg_color == (255, 255, 255):
                     bg_color = ('#000000')
+        if ev.type == MOUSEBUTTONUP:
+            if ev.button == 1:
+                text = 'AHAHA'
+            
 
     # Update
     dt = clock.get_time()/1000
+
 
     if keys[K_d] or keys[K_RIGHT]:
         pos_X = pos_x + 1 * dt
