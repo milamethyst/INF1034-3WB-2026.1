7dde12f68bffd638abb0b317b4c40103bc1016e5
Camila <camisalgado2005@gmail.com>
2026-05-11 12:57:35 -0300

    tree frac

--- Atividade-9/main.py
+++ Atividade-9/main.py
@@ -6,7 +6,8 @@ t = Turtle()
 t.speed(0)
 
 def main():
-    star_frac(t, 100)
+    t.lt(90)
+    # tree_frac(t, 150, 30, 1)
 
 def sq_frac(t, size, step = 50):
     if size == 0 or step == 0:
@@ -16,7 +17,6 @@ def sq_frac(t, size, step = 50):
     t.lt(10)
     sq(t, size)
     sq_frac(t, size - 1, step - 1)
-
 
 def sq(t, size):
     t.pd()
@@ -31,6 +31,24 @@ def star_frac(t, size):
         t.fd(size)
         star_frac(t, size / 3)
         t.lt(216)
+
+# def tree_frac(t, size, angle, level):
+    # if size < 40:
+    #     return
+
+    # t.fd(size)
+    # t.rt(angle)
+    # t.fd(size)
+    # tree_frac(t, size * 0.08, angle, level - 1)
+    # t.back(size)
+
+    # t.lt(angle * 2)
+    # t.fd(size)
+    # tree_frac(t, size * 0.08, angle, level - 1)
+    # t.back(size)
+
+    # t.rt(angle)
+    # t.back(size)
 
 # def random_color():
 #     return (randint(0, 255), randint(0, 255), randint(0, 255))
